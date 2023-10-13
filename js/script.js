2bc1f9a6bf44bb1f4c4daec8cf1ed06b7ade0a19
Endriw Braga <endriwbragalisboa@gmail.com>
2023-10-13 05:15:30 -0300

    Primeira implementação de checkbox

--- js/script.js
+++ js/script.js
@@ -10,13 +10,25 @@ const fetchApi = (value) => {
         console.log(data);
         return data;
     })   
-
     return result;
+}
+
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode'];
+
+const buildResult = (result) => {
+    const newObject = {};
+    keys.map((key) => document.getElementById(key)).map((elem) =>{
+        elem.checked && (newObject[elem.name] = result[elem.name]);
+    });
+
+    return newObject
 }
 
 btnGo.addEventListener('click', async (event)=>{
     event.preventDefault();
     const result = await fetchApi(characterId.value);
-    contentPre.textContent = `${JSON.stringify(result, undefined, 2)}`;
+    //contentPre.textContent = `${JSON.stringify(result, undefined, 2)}`;
+
+    contentPre.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
     image.src = `${result.image}`;
 });
